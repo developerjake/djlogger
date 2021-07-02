edbcbefde7ab1f2818b792f742ca4b18d7915292
Jake Stokes <contactjake@developerjake.com>
2021-07-02 14:36:00 +0100

    FEAT add more exports & a console overwrite method

--- src/app.ts
+++ src/app.ts
@@ -45,7 +45,7 @@ const logFormatter = (logFunction: LeveledLogMethod) => {
   }
 }
 
-const getLogger = (wipePreviousLogs = true, locale = 'en-GB'): Logger => {
+export const getLogger = (wipePreviousLogs = true, locale = 'en-GB'): Logger => {
   const options = { ...(wipePreviousLogs && { flags: 'w' }) };
 
   const formatter = printf(({ level, message, timestamp }) => {
@@ -82,4 +82,25 @@ const getLogger = (wipePreviousLogs = true, locale = 'en-GB'): Logger => {
   return logger;
 }
 
-export default getLogger;
+/**
+ * Overwrite the nodeJS console logging methods with a colourized winston logger.
+ * This includes transports to three files in the app's root:  
+ * * info.log
+ * * debug.log
+ * * silly.log
+ * 
+ * @param  {boolean} [wipePreviousLogs=true] whether to wipe old logs on start-up or not
+ * @param  {string}  [locale='en-GB'] the locale used to create timestamps
+ */
+export const overwriteConsole = (wipePreviousLogs = true, locale = 'en-GB') => {
+  const logger = getLogger(wipePreviousLogs, locale);
+
+  console.error = logFormatter(logger.error.bind(logger));
+  console.log = logFormatter(logger.info.bind(logger));
+  console.info = logFormatter(logger.info.bind(logger));
+  console.debug = logFormatter(logger.debug.bind(logger));
+  console.warn = logFormatter(logger.warn.bind(logger));
+}
+
+export const logger = getLogger();
+export const { log, info, debug, silly, warn, error, table } = logger;
