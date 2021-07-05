78c69d0ea9256689970b4b9d746c9ff1d2adab13
Jake Stokes <contactjake@developerjake.com>
2021-07-05 01:34:24 +0100

    FEAT clean up formats & add separate warn-level

--- app/logger.ts
+++ app/logger.ts
@@ -22,7 +22,7 @@ if (env.error) {
 const { combine, timestamp, printf } = format;
 
 const logFormatter = (logFunction: LeveledLogMethod) => {
-  // winston can't handle comma-separated logging
+  // change winston logging to allow comma-separate args
   return(...args: any[]) => {
     const result: string[] = [];
     args.forEach(arg => {
@@ -46,17 +46,22 @@ export const getLogger = (wipePreviousLogs = true, locale = 'en-GB'): Logger => 
     const timeString = date.toLocaleTimeString(locale);
     return `${dateString} ${timeString} ${level.padStart(5)}: ${message}`;
   });
-
+  const baseFormat = combine(
+    format.splat(),
+    format.simple(),
+    timestamp(),
+    format.metadata({ fillExcept: ['message', 'level', 'timestamp'] })
+  );
   const fileFormat = combine(fileFormatter);
   const consoleFormat = combine(format.colorize({ level: true }), consoleFormatter);
 
   const winstonLogger = createLogger({
-    level: 'info',
-    format: combine(format.splat(), format.simple(), timestamp()),
+    format: baseFormat,
     transports: [
       new transports.File({ filename: 'logs/info.log', level: 'info', options, format: fileFormat }),
       new transports.File({ filename: 'logs/debug.log', level: 'debug', options, format: fileFormat }),
       new transports.File({ filename: 'logs/silly.log', level: 'silly', options, format: fileFormat }),
+      new transports.File({ filename: 'logs/warn.log', level: 'warn', options, format: fileFormat }),
       new transports.File({ filename: 'logs/error.log', level: 'error', options, format: fileFormat }),
       new transports.Console({ level: process.env.LOG_LEVEL || 'debug', format: consoleFormat })
     ]
@@ -64,11 +69,11 @@ export const getLogger = (wipePreviousLogs = true, locale = 'en-GB'): Logger => 
 
   const logger = {
     log: console.log,
-    info: logFormatter(winstonLogger.info.bind(winstonLogger)),
-    debug: logFormatter(winstonLogger.debug.bind(winstonLogger)),
-    silly: logFormatter(winstonLogger.silly.bind(winstonLogger)),
-    warn: logFormatter(winstonLogger.warn.bind(winstonLogger)),
-    error: logFormatter(winstonLogger.error.bind(winstonLogger)),
+    info: logFormatter((...args: any[]) => winstonLogger.info(args)),
+    debug: logFormatter((...args: any[]) => winstonLogger.debug(args)),
+    silly: logFormatter((...args: any[]) => winstonLogger.silly(args)),
+    warn: logFormatter((...args: any[]) => winstonLogger.warn(args)),
+    error: logFormatter((...args: any[]) => winstonLogger.error(args)),
     table: console.table
   }
 
@@ -80,18 +85,18 @@ export const getLogger = (wipePreviousLogs = true, locale = 'en-GB'): Logger => 
  * This includes transports to three files in the app's root:  
  * * info.log
  * * debug.log
- * * silly.log
+ * * warn.log
+ * * error.log
  * 
  * @param  {boolean} [wipePreviousLogs=true] whether to wipe old logs on start-up or not
  * @param  {string}  [locale='en-GB'] the locale used to create timestamps
  */
-export const overwriteConsole = (wipePreviousLogs = true, locale = 'en-GB') => {
+export const overwriteConsole = (wipePreviousLogs=true, locale='en-GB') => {
   const logger = getLogger(wipePreviousLogs, locale);
-
-  console.error = logFormatter(logger.error.bind(logger));
-  console.info = logFormatter(logger.info.bind(logger));
-  console.debug = logFormatter(logger.debug.bind(logger));
-  console.warn = logFormatter(logger.warn.bind(logger));
+  console.info = logger.info;
+  console.debug = logger.debug;
+  console.warn = logger.warn;
+  console.error = logger.error;
 }
 
 export const logger = getLogger();
